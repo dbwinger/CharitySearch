bbee34e428dc6118c1bf0d4df8a49822dc1653a5
Daryl Winger <dbwinger@gmail.com>
2012-03-16 13:43:03 -0400

    add initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharitySearch::Application.config.secret_token = '07529b72320ac09281001336829a2544cbabf61c7e6887ad0574b5445ee45311634a99e1a54f5b1d1fc4b0ad16cf014a888e08a72db379627314c57aa7a861a6'
